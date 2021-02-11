ccddb230e4bf0adb1e1aaa57b5a12980913d3c7e
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-11 12:58:03 -0500

    tiitle and room create

--- server.js
+++ server.js
@@ -48,9 +48,9 @@ app.post('/messages', (req, res) => {
     res.sendStatus(200);
   })
 })
-var roomno = 1;
+
 io.on("connection", socket => {
-  io.to('room1').to('room2').to('room3').emit('some event');
+  console.log("user connected");
 
 
   socket.on("disconnect", function() {
